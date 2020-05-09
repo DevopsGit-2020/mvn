c25d8a935a724099390136c802f5469eee484960
root <root@ip-172-31-83-250.ec2.internal>
2020-05-09 16:23:50 +0000

    mvn

---
+++ src/main/java/mvn/App.java
@@ -0,0 +1,13 @@
+package mvn;
+
+/**
+ * Hello maven_world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello maven_world!" );
+    }
+}
